9e23df9f604a0e1695d93330a2c95d1b0abf2015
diliska <diliska@gmail.com>
2019-10-17 20:32:27 +0600

    Printstr commit 1710 2-0

--- printstr.go
+++ printstr.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func PrintStr(str string) {
 	strchanged := []rune(str)
 
-	for i := 0; i < len(str); i++ {
-		z01.PrintRune(strchanged[i])
+	for _, s := range strchanged {
+		z01.PrintRune(s)
 	}
 	z01.PrintRune(10)
 }
