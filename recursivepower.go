ea922471417a44042a9e97c0eb45a55621306574
diliska <diliska@gmail.com>
2019-10-21 14:18:34 +0600

    Recursivepowercommit 2110 2-0

--- recursivepower.go
+++ recursivepower.go
@@ -3,6 +3,8 @@ package piscine
 func RecursivePower(nb int, power int) int {
 	if power == 0 {
 		return 1
+	} else if power < 0 {
+		return 0
 	} else {
 		res := nb * RecursivePower(nb, power-1)
 		return res
