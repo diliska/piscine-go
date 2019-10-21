e8c2d63592a8dc20091307e399e8897c6540ccb0
diliska <diliska@gmail.com>
2019-10-21 14:30:22 +0600

    Fibonacci commit 2110

---
+++ fibonacci.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Fibonacci(index int) int {
+
+	if index < 0 {
+		return -1
+	} else if index == 0 {
+		return 0
+	} else if index == 1 {
+		return 1
+	} else {
+		k := Fibonacci(index-1) + Fibonacci(index-2)
+		return k
+	}
+
+}
