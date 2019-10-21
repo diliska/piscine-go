fdd117746553e1977a36d04fe2704eb4ab746f71
diliska <diliska@gmail.com>
2019-10-21 14:39:41 +0600

    sqrt commit 2110

--- sqrt.go
+++ sqrt.go
@@ -2,17 +2,20 @@ package piscine
 
 func Sqrt(nb int) int {
 
-if nb<=0 {return 0} else {
+	if nb <= 0 {
+		return 0
+	} else {
 
-res:=0
+		res := 0
 
-for i:=1; i<=nb; i++ {
+		for i := 1; i <= nb; i++ {
 
-	if i*i==nb {
-		res=i
-		break
+			if i*i == nb {
+				res = i
+				break
+			}
+		}
+
+		return res
 	}
-}
-
-return res}
 }
