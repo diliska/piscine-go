a0fbe10b9058e70232787129ecd5fab940c894de
diliska <diliska@gmail.com>
2019-10-21 13:53:04 +0600

    Iterativefactorial commit 2110 2-0

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,15 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-
-	if nb == 0 {
-		return 1
+	if (nb < 0) || (nb > 15) {
+		return 0
 	} else {
+		if nb == 0 {
+			return 1
+		} else {
+			fact := nb * IterativeFactorial(nb-1)
 
-		fact := nb * IterativeFactorial(nb-1)
-
-		return fact
+			return fact
+		}
 	}
 }
