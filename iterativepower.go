8b99b21fcd590dbf3373fb83e977f8d41ffc05f6
diliska <diliska@gmail.com>
2019-10-21 14:09:14 +0600

    Iterativepower commit 2110

--- iterativepower.go
+++ iterativepower.go
@@ -2,12 +2,12 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 
-	if (power <0) {
+	if power < 0 {
 		return 0
 	} else {
-		res:=1;
-		for i:=1; i<=power; i++ {
-			res=res*nb
+		res := 1
+		for i := 1; i <= power; i++ {
+			res = res * nb
 		}
 		return res
 	}
