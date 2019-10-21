79c4e03092990d3486acecea80fd5fb6d78f9356
diliska <diliska@gmail.com>
2019-10-21 15:16:08 +0600

    Findnetprime commit 2110

---
+++ findnextprime.go
@@ -0,0 +1,38 @@
+package piscine
+
+
+func IsPrime1(nb int) bool {
+
+	if (nb == 0) || (nb == 1) {
+		return false
+	} else {
+		flag := false
+		kol := 1
+		for i := 1; i <= nb-1; i++ {
+			if nb%i == 0 {
+				kol = kol + 1
+			}
+		}
+		if kol == 2 {
+			flag = true
+		}
+		return flag
+	}
+}
+
+
+func FindNextPrime(nb int) int {
+	res:=0
+	if nb<=1 {return 2} else {
+
+	for i:=nb; i<=2*nb; i++ {
+
+		if IsPrime1(i) {
+			res=i
+			break
+		}
+	}
+	
+	return res
+}	
+}
