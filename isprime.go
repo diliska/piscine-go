2358b136af2b5692ca6ee82e24cb072fbcb58a0f
diliska <diliska@gmail.com>
2019-10-21 15:01:55 +0600

    Isprime commit 2110

---
+++ isprime.go
@@ -0,0 +1,20 @@
+package piscine
+
+func IsPrime(nb int) bool {
+
+	if (nb == 0) || (nb == 1) {
+		return false
+	} else {
+		flag := false
+		kol := 1
+		for i := 1; i <= nb-1; i++ {
+			if nb%i == 0 {
+				kol = kol + 1
+			}
+		}
+		if kol > 2 {
+			flag = true
+		}
+		return flag
+	}
+}
