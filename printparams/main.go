c8f13b30638c949d4d5e2c5312b14a8acd2682b5
diliska <diliska@gmail.com>
2019-10-23 11:18:51 +0600

    Printparams commit 2310

--- printparams/main.go
+++ printparams/main.go
@@ -7,11 +7,13 @@ func main() {
 
 	arguments := os.Args
 
-	for _, k := range arguments {
-		str := k
-		for _, i := range str {
-			z01.PrintRune(i)
+	for index, k := range arguments {
+		if index != 0 {
+			str := k
+			for _, i := range str {
+				z01.PrintRune(i)
+			}
+			z01.PrintRune(10)
 		}
-		z01.PrintRune(10)
 	}
 }
